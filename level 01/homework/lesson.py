4395191118c75a783009d1f247efb56f52a091c4
marr0318 <Mariamgelashvili1803@gmail.com>
2024-06-25 13:46:05 +0400

    commit

---
+++ level 01/homework/lesson.py
@@ -0,0 +1,6 @@
+name="mariami"
+surname="gelashvili"
+age=12
+height=163
+
+print("me var" +" " + name + " " + surname + " " + "wlovanebit var" + " " + str(age) + " " + "simaglit var" + " "+ str(height))
